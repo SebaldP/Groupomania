3b0617e3cb83f1da08c4abb30e14b42d741be023
Sebald <68822652+SebaldP@users.noreply.github.com>
2021-06-02 10:11:16 +0200

    FrontEnd - Vue CLI, VueX, Vue-router, Axios
    Problème de git push

---
+++ FrontEnd - Vue CLI, VueX, Vue-router, Axios/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <navbar />
+    <router-view />
+    <message-popup :isVisible="isVisible" :text="message" />
+    <c-footer />
+  </v-app>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Navbar from "./components/Navbar";
+import CFooter from "./components/Footer";
+import MessagePopup from "./components/MessagePopup";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    CFooter,
+    MessagePopup,
+  },
+  watch: {
+    isVisible() {
+      console.log(this.message);
+      return this.message ? true : false;
+    },
+  },
+  computed: {
+    ...mapGetters(["message"]),
+    ...mapGetters(["user"]),
+  },
+};
+</script>
